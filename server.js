dec093c79a0595df7eac4aa505123c554ae452f8
ritikaswal27 <ritikkaswal2705@gmail.com>
2023-03-31 23:51:14 +0530

    logger file

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const { logger } = require("./middleware/logger");
 const PORT = process.env.PORT || 3500;
 
-app.use("/", express.static(path.join(__dirname, "/public")));
+app.use(logger);
+
+app.use(express.json());
+
+// app.use("/", express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
+
+app.use("/", require("./routes/root"));
+
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.sendFile({ message: "404 not found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
+});
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
